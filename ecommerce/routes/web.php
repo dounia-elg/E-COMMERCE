70d21917a111db77be3411cf3c1124f7ce7852af
dounia-elg <dounia.elg20@gmail.com>
2025-03-06 12:40:42 +0000

    Add User Profile Routes

--- ecommerce/routes/web.php
+++ ecommerce/routes/web.php
@@ -4,6 +4,7 @@
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\EmailVerificationController;
+use App\Http\Controllers\UserProfileController;
 
 // Authentication Routes
 Route::middleware('guest')->group(function () {
@@ -12,6 +13,7 @@ Route::middleware('guest')->group(function () {
     Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
     Route::post('/login', [LoginController::class, 'login']);
 });
+
 
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
@@ -25,4 +27,15 @@ Route::middleware('auth')->group(function () {
     Route::post('/email/verification-notification', [EmailVerificationController::class, 'resend'])
         ->middleware(['throttle:6,1'])
         ->name('verification.resend');
+});
+
+
+// User Profile Routes
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/profile', [UserProfileController::class, 'show'])->name('profile.show');
+    Route::get('/profile/edit', [UserProfileController::class, 'edit'])->name('profile.edit');
+    Route::put('/profile', [UserProfileController::class, 'update'])->name('profile.update');
+    Route::get('/profile/change-password', [UserProfileController::class, 'changePassword'])->name('profile.change-password');
+    Route::put('/profile/change-password', [UserProfileController::class, 'updatePassword'])->name('profile.update-password');
+    Route::get('/profile/orders', [UserProfileController::class, 'orders'])->name('profile.orders');
 });
